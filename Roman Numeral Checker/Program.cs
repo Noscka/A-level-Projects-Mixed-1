19a4f171040584fc0a0ff50dfb9289047ca85494
Adam Krawczyk <Adam882200@gmail.com>
2022-12-12 13:18:14 +0000

    Going lunch

---
+++ Roman Numeral Checker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Numeral_Checker
+{
+	internal class Program
+	{
+		Dictionary<char, int> romanNumbersDictionary = new() {
+		{'I',1},
+		{'V',5}, {
+			'X',
+			10
+		}, {
+			'L',
+			50
+		}, {
+			'C',
+			100
+		}, {
+			'D',
+			500
+		}, {
+			'M',
+			1000
+		}
+		};
+
+		static string RomanNumeralValidation(string inputNumberal)
+		{
+
+
+			return "";
+		}
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}
